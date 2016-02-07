6bffd6c3bd4df5167ae49a36fc6ca335b2c36637
Cesar Villalobos <Cesarvh@Cesars-MacBook-Pro.local>
2016-02-06 21:50:01 -0800

    Fixed random module dependency

--- takeShots.py
+++ takeShots.py
@@ -1,22 +1,25 @@
 import time
 import math
 import os
+import random
 
-def takeAShot(interval, numShots, random=False):
+def takeAShot(interval, numShots, randomizeParam=False):
 	""" 
 	Function that tells you when to take a shot based on a given interval.
 	@param interval: The time interval you want to take a shot in, in minutes
 	@param numShots: The number of shots you want to take 
-	@param random: randomizes the intervals in which the shots should be taken. The interval will 
+	@param randomizeParam: randomizes the intervals in which the shots should be taken. The interval will 
 		result in a number from 0-10.
 	Linux only, I think. If 
 	"""	
 	for shot in range(numShots):
 		flag = False
 		start = time.time()
-		if (random == True):
-			rand = math.floor(random.random() * 10)
+		if (randomizeParam == True):
+			rand = math.floor(random.random())
+			rand *= 10 + 1
 			interval = rand	
+			print interval
 		while (flag == False):
 			end = time.time()
 			if (math.floor(end - start) == 60.0 * interval):
